b5d9733ed3cf854c041946b30d9c19d8a338069a
AlexandrIvanov <sanek-pi202@yandex.ru>
2016-11-07 08:31:10 +0300

    Добавлена общая структура проектов.

---
+++ VS Solution/Apiary/Apiary.Tests/UnitTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+
+namespace Apiary.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(2*2, 4);
+        }
+    }
+}
---
+++ VS Solution/Apiary/Apiary.Tests/subfolder/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+
+namespace Apiary.Tests.subfolder
+{
+    [TestClass]
+    public class Class1
+    {
+        [TestMethod]
+        public void fdfdfd()
+        {
+            string str = new Apiary.Class1().GetString();
+            Assert.AreEqual("String from model", str);
+        }
+    }
+}
