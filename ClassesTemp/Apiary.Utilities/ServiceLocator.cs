60e0f4d1e6a44f2b83895c10569d92ebbbf0ab57
AlexandrIvanov <sanek-pi202@yandex.ru>
2016-11-20 23:08:39 +0300

    (Visual Studio Code) Разработан  ServiceLocator - рабочий вариант

---
+++ ClassesTemp/Apiary.Utilities/ServiceLocator.cs
@@ -0,0 +1,81 @@
+namespace Apiary.Utilities
+{
+    using System;
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// Простая реализация паттерна service locator.
+    /// </summary>
+    public class ServiceLocator
+    {
+        /// <summary>
+        /// Единственный экземпляр service locator'а.
+        /// </summary>
+        private static ServiceLocator instance;
+
+        /// <summary>
+        /// Объект для синхронизации потоков.
+        /// </summary>
+        private static readonly object lockObject = new object();
+
+        /// <summary>
+        /// Зарегистрированные сервисы.
+        /// </summary>
+        private readonly Dictionary<Type, object> registeredServices
+            = new Dictionary<Type, object>();
+
+        /// <summary>
+        /// Получить единственный экземпляр service locator'а.
+        /// </summary>
+        /// <returns>Экземпляр service locator'а.</returns>
+        public static ServiceLocator Instance
+        {
+            get
+            {
+                if (ServiceLocator.instance == null)
+                {
+                    lock (lockObject)
+                    {
+                        if (ServiceLocator.instance == null)
+                        {
+                            ServiceLocator.instance = new ServiceLocator;
+                        }
+                    }
+                }
+
+                return ServiceLocator.instance;            
+            }
+        }
+
+        /// <summary>
+        /// Конструктор для предотвращения прямого создания извне.
+        /// </summary>
+        private ServiceLocator() {}
+
+        /// <summary>
+        /// Зарегистрировать сервис.
+        /// </summary>
+        /// <param name="instance">Экземпляр сервиса.</param>
+        public void RegisterService<T>(T instance)
+        {
+            this.registeredServices[typeof(T)] = instance;
+        }
+
+        /// <summary>
+        /// Получить зарегистрированный сервис.
+        /// </summary>
+        /// <returns>Зарегистрированный сервис.</returns>
+        public T GetService<T>()
+        {
+            try
+            {
+                return (T)this.registeredServices[typeof(T)];
+            }
+            catch (KeyNotFoundException)
+            {
+                throw new InvalidOperationException(
+                    $"Попытка получить незарегистрированный сервис {typeof(T).Name}.");
+            }
+        }
+    }
+}
